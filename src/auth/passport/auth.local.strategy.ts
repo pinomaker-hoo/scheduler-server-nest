f4c9639ae8fd53cf8c1cc8f0f482d66fcea42082
pino._.hoo <inhoo23@naver.com>
2022-10-05 15:27:14 +0900

    add : local passport set

---
+++ src/auth/passport/auth.local.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../application/auth.service';
+import { User } from '../domain/user.entity';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      usernameField: 'id',
+      passwordField: 'password',
+    });
+  }
+  async validate(id: string, password: string): Promise<User> {
+    return await this.authService.localLogin(id, password);
+  }
+}
