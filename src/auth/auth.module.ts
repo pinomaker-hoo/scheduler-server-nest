f60f3094d571034de33d99084e97763c80b20639
pino._.hoo <inhoo23@naver.com>
2022-10-05 15:27:03 +0900

    add : jwt set

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthService } from './application/auth.service';
 import { UserRepository } from './infrastructure/user.repository';
+import { AuthController } from './ui/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
-  providers: [],
-  controllers: [],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRESIN'),
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ src/auth/passport/auth.jwt.passport.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../application/auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: any) => {
+          return request.headers.accesstoken;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+  async validate(payload) {
+    return await this.authService.getById(payload.idx);
+  }
+}
