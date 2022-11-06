68c86c6b0f44176f37ecd5bc64a3586ca94fb383
pino._.hoo <inhoo23@naver.com>
2022-11-07 04:51:30 +0900

    fix : Group-todos api refactoring

--- src/groupTodos/application/groupTodos.service.ts
+++ src/groupTodos/application/groupTodos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { GroupService } from 'src/group/application/group.service'
 import { Group } from 'src/group/domain/group.entity'
 import { GroupTodos } from '../domain/groupTodos.entity'
@@ -12,18 +12,28 @@ export class GroupTodosService {
     private readonly groupService: GroupService,
   ) {}
   async getTodosList(idx: number): Promise<GroupTodos[]> {
-    const group: Group = await this.groupService.findGroupByIdx(idx)
-    return await this.groupTodosRepository.find({ where: { group } })
+    try {
+      const group: Group = await this.groupService.findGroupByIdx(idx)
+      return await this.groupTodosRepository.find({ where: { group } })
+    } catch (err) {
+      console.log(err)
+      throw new HttpException('BAD', HttpStatus.BAD_REQUEST)
+    }
   }
 
   async saveTodos(idx: number, body: SaveTodosDto): Promise<GroupTodos> {
-    const group: Group = await this.groupService.findGroupByIdx(idx)
-    const todos = this.groupTodosRepository.create({
-      group,
-      date: body.date,
-      title: body.title,
-      place: body.place,
-    })
-    return await this.groupTodosRepository.save(todos)
+    try {
+      const group: Group = await this.groupService.findGroupByIdx(idx)
+      const todos = this.groupTodosRepository.create({
+        group,
+        date: body.date,
+        title: body.title,
+        place: body.place,
+      })
+      return await this.groupTodosRepository.save(todos)
+    } catch (err) {
+      console.log(err)
+      throw new HttpException('BAD', HttpStatus.BAD_REQUEST)
+    }
   }
 }
--- src/groupTodos/ui/groupTodos.controller.ts
+++ src/groupTodos/ui/groupTodos.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { ApiResponse } from 'src/common/dto/response.dto'
 import { GroupTodosService } from '../application/groupTodos.service'
 import { GroupTodos } from '../domain/groupTodos.entity'
 import { SaveTodosDto } from '../dto/groupTodos.save.dto'
@@ -8,15 +9,22 @@ export class GroupTodosController {
   constructor(private readonly groupTodosService: GroupTodosService) {}
 
   @Get('/:id')
-  async getTodos(@Param('id') idx: string): Promise<GroupTodos[]> {
-    return await this.groupTodosService.getTodosList(Number(idx))
+  async getTodos(@Param('id') idx: string) {
+    const response = await this.groupTodosService.getTodosList(Number(idx))
+    return ApiResponse.of({
+      data: response,
+      message: 'Success Get Group Todos',
+      statusCode: 200,
+    })
   }
 
   @Post('/:id')
-  async saveTodos(
-    @Param('id') idx: string,
-    @Body() body: SaveTodosDto,
-  ): Promise<GroupTodos> {
-    return await this.groupTodosService.saveTodos(Number(idx), body)
+  async saveTodos(@Param('id') idx: string, @Body() body: SaveTodosDto) {
+    const response = await this.groupTodosService.saveTodos(Number(idx), body)
+    return ApiResponse.of({
+      data: response,
+      message: 'Success Save Group Todos',
+      statusCode: 200,
+    })
   }
 }
