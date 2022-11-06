6937add6d0acef5acd740dbf9bee7a7d69e6f664
pino._.hoo <inhoo23@naver.com>
2022-11-07 04:54:48 +0900

    fix : todos Api refactoring

--- src/todos/application/todos.service.ts
+++ src/todos/application/todos.service.ts
@@ -1,4 +1,4 @@
-import { Body, Injectable } from '@nestjs/common'
+import { Body, HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { User } from 'src/auth/domain/user.entity'
 import { Todos } from '../domain/todos.entity'
 import { SaveTodosDto } from '../dto/todos.save.dto'
@@ -9,16 +9,26 @@ export class TodosService {
   constructor(private readonly todosRepository: TodosRepository) {}
 
   async getTodosList(user: User): Promise<Todos[]> {
-    return await this.todosRepository.find({ where: { user } })
+    try {
+      return await this.todosRepository.find({ where: { user } })
+    } catch (err) {
+      console.log(err)
+      throw new HttpException('BAD', HttpStatus.BAD_REQUEST)
+    }
   }
 
   async saveTodos(user: User, body: SaveTodosDto): Promise<Todos> {
-    const todos = this.todosRepository.create({
-      user,
-      date: body.date,
-      title: body.title,
-      place: body.place,
-    })
-    return await this.todosRepository.save(todos)
+    try {
+      const todos = this.todosRepository.create({
+        user,
+        date: body.date,
+        title: body.title,
+        place: body.place,
+      })
+      return await this.todosRepository.save(todos)
+    } catch (err) {
+      console.log(err)
+      throw new HttpException('BAD', HttpStatus.BAD_REQUEST)
+    }
   }
 }
--- src/todos/ui/todos.controller.ts
+++ src/todos/ui/todos.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common'
 import { JwtGuard } from 'src/auth/passport/auth.jwt.guard'
+import { ApiResponse } from 'src/common/dto/response.dto'
 import { TodosService } from '../application/todos.service'
 import { Todos } from '../domain/todos.entity'
 import { SaveTodosDto } from '../dto/todos.save.dto'
@@ -10,13 +11,23 @@ export class TodosController {
 
   @Post()
   @UseGuards(JwtGuard)
-  async saveTodos(@Req() req, @Body() body: SaveTodosDto): Promise<Todos> {
-    return await this.todosService.saveTodos(req.user, body)
+  async saveTodos(@Req() req, @Body() body: SaveTodosDto) {
+    const response = await this.todosService.saveTodos(req.user, body)
+    return ApiResponse.of({
+      data: response,
+      message: 'Success Save Todos',
+      statusCode: 200,
+    })
   }
 
   @Get()
   @UseGuards(JwtGuard)
-  async getTodosList(@Req() req): Promise<Todos[]> {
-    return await this.todosService.getTodosList(req.user)
+  async getTodosList(@Req() req) {
+    const response = await this.todosService.getTodosList(req.user)
+    return ApiResponse.of({
+      data: response,
+      message: 'Success Get Todos',
+      statusCode: 200,
+    })
   }
 }
