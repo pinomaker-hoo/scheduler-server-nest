d59e2acecec9f11b3a36142f74495ac87fa26479
pino._.hoo <inhoo23@naver.com>
2022-11-07 04:57:43 +0900

    add : Server Option

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  console.log(process.env.NODE_SERVER_PORT)
+  app.use('/src/source/img', express.static('./src/source/img'))
+  app.use(express.json({ limit: '50mb' }))
+  app.use(express.urlencoded({ limit: '50mb', extended: true }))
   await app.listen(process.env.NODE_SERVER_PORT)
 }
 bootstrap()
